9bc3e4374c42328ca548ddc5effc69694f9db448
TonyTang <tonytang@uber.com>
2022-11-05 11:09:49 -0700

    Enforce Goolge Java Format

--- samples/database-v1/src/main/java/com/example/MainActivity.java
+++ samples/database-v1/src/main/java/com/example/MainActivity.java
@@ -8,30 +8,31 @@ import android.widget.SimpleCursorAdapter;
 
 public class MainActivity extends ListActivity {
 
-	private Cursor employees;
-	private MyDatabase db;
+  private Cursor employees;
+  private MyDatabase db;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-		db = new MyDatabase(this);
-		employees = db.getEmployees(); // you would not typically call this on the main thread
+    db = new MyDatabase(this);
+    employees = db.getEmployees(); // you would not typically call this on the main thread
 
-		ListAdapter adapter = new SimpleCursorAdapter(this, 
-				android.R.layout.simple_list_item_1, 
-				employees, 
-				new String[] {"FirstName"}, 
-				new int[] {android.R.id.text1});
+    ListAdapter adapter =
+        new SimpleCursorAdapter(
+            this,
+            android.R.layout.simple_list_item_1,
+            employees,
+            new String[] {"FirstName"},
+            new int[] {android.R.id.text1});
 
-		getListView().setAdapter(adapter);
-	}
+    getListView().setAdapter(adapter);
+  }
 
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		employees.close();
-		db.close();
-	}
-
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    employees.close();
+    db.close();
+  }
 }
